b967afd5ffd48c7ec237d440e4b2801b56592e99
ifgyong <fgyong@fgyong.cn>
2019-04-30 14:52:38 +0800

    m_2

--- Swift/Swift_rx/Swift_rx/ViewController.swift
+++ Swift/Swift_rx/Swift_rx/ViewController.swift
@@ -24,6 +24,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         textField.placeholder = "默认输入文字label展示文字23"
 
+
         let test =  textField.rx
             test.text.orEmpty
                 .filter({ (Str) -> Bool in
