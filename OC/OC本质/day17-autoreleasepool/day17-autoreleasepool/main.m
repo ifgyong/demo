771d327d410a2bab412201f1cc2089eed3c52fdf
ifgyong <fgyong@fgyong.cn>
2019-08-06 09:52:11 +0800

    fix

--- OC/OC本质/day17-autoreleasepool/day17-autoreleasepool/main.m
+++ OC/OC本质/day17-autoreleasepool/day17-autoreleasepool/main.m
@@ -11,19 +11,29 @@
 extern void _objc_autoreleasePoolPrint(void);
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {//r1 = push()
-
-		FYPerson *p = [[[FYPerson alloc]init] autorelease];
-		FYPerson *p2 = [[[FYPerson alloc]init] autorelease];
-		@autoreleasepool {//r1 = push()
-			
-			FYPerson *p3 = [[[FYPerson alloc]init] autorelease];
-			FYPerson *p4 = [[[FYPerson alloc]init] autorelease];
-			
-			_objc_autoreleasePoolPrint();
-			printf("\n--------------\n");
-		}//pop(r1)
-		_objc_autoreleasePoolPrint();
-		printf("\n--------------\n");
+		//转化成c++文件
+//xcrun -sdk iphoneos clang -arch arm64 -rewrite-objc -f  main.m
+//		FYPerson *p = [[[FYPerson alloc]init] autorelease];
+//		FYPerson *p2 = [[[FYPerson alloc]init] autorelease];
+//		@autoreleasepool {//r1 = push()
+//
+//			FYPerson *p3 = [[[FYPerson alloc]init] autorelease];
+//			FYPerson *p4 = [[[FYPerson alloc]init] autorelease];
+//
+//			_objc_autoreleasePoolPrint();
+//			printf("\n--------------\n");
+//		}//pop(r1)
+//		_objc_autoreleasePoolPrint();
+//		printf("\n--------------\n");
+		
+		
+		FYPerson *p=[[FYPerson alloc]init];
+		FYPerson *p2 =[p retain];
+		//code here
+		[p release];
+		[p2 release];
+		
+		
 	}//pop(r1)
 	return 0;
 }
@@ -32,16 +42,19 @@ int main(int argc, const char * argv[]) {
 //	__AtAutoreleasePool __autoreleasepool;
 //}
 
+/*
+ 
+struct __AtAutoreleasePool {
+	__AtAutoreleasePool() {//构造函数 生成结构体变量的时候调用
+		atautoreleasepoolobj = objc_autoreleasePoolPush();
+	}
+	~__AtAutoreleasePool() {//析构函数 销毁的时候调用
+		objc_autoreleasePoolPop(atautoreleasepoolobj);
+	}
+	void * atautoreleasepoolobj;
+};
+__AtAutoreleasePool pool = objc_autoreleasePoolPush();
+FYPerson *p = [[FYPerson alloc]init];
+objc_autoreleasePoolPop(pool)
 
-//struct __AtAutoreleasePool {
-//	__AtAutoreleasePool() {//构造函数 生成结构体变量的时候调用
-//		atautoreleasepoolobj = objc_autoreleasePoolPush();
-//	}
-//	~__AtAutoreleasePool() {//析构函数 销毁的时候调用
-//		objc_autoreleasePoolPop(atautoreleasepoolobj);
-//	}
-//	void * atautoreleasepoolobj;
-//};
-//__AtAutoreleasePool pool = objc_autoreleasePoolPush();
-//FYPerson *p = [[FYPerson alloc]init];
-//objc_autoreleasePoolPop(pool)
+ */
