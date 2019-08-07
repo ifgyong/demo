b7db7372869f56e51a1188921fce960739f088ea
ifgyong <fgyong@fgyong.cn>
2019-08-07 22:31:31 +0800

    启动优化准备

---
+++ OC/OC本质/day19-启动时间优化/day19-启动时间优化/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  day19-启动时间优化
+//
+//  Created by fgy on 2019/8/7.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OC/OC本质/day19-启动时间优化/day19-启动时间优化/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  day19-启动时间优化
+//
+//  Created by fgy on 2019/8/7.
+//  Copyright © 2019 test. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
