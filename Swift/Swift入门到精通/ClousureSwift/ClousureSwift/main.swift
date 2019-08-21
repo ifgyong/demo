d8eaab28eb08ac00cddf878d933091ca363f0b82
ifgyong <fgyong@fgyong.cn>
2019-08-21 22:58:22 +0800

    closure

--- Swift/Swift入门到精通/ClousureSwift/ClousureSwift/main.swift
+++ Swift/Swift入门到精通/ClousureSwift/ClousureSwift/main.swift
@@ -25,21 +25,70 @@ import Foundation
 //
 //print(array)
 //typeAlias Fn = (Int) -> Int;
-typealias Clo = (Int) -> Int
-func getFunc () -> Clo {
-	var num = 5 //局部变量 赋值到了堆空间来保证变量的值。每次调用都访问了堆空间了。
-	func plus(_ v:Int) ->Int{
-		num += v
-		return num
-	}
-	return plus
-}
-var f = getFunc()
-print(f(1)) //5+1 = 6
-print(f(2)) //6 + 2 =8
+//typealias Clo = (Int) -> Int
+//func getFunc () -> Clo {
+//    var num = 5 //局部变量 赋值到了堆空间来保证变量的值。每次调用都访问了堆空间了。
+//    func plus(_ v:Int) ->Int{
+//        num += v
+//        return num
+//    }
+//    return plus
+//}
+//var f = getFunc()
+//print(f(1)) //5+1 = 6
+//print(f(2)) //6 + 2 =8
+//
+//var f2 = getFunc()//每次调用都会调用新的堆空间的 num，他们是分开没联系的。
+//print(f2(3))// 5 + 3 = 8
 
-var f2 = getFunc()//每次调用都会调用新的堆空间的 num，他们是分开没联系的。
-print(f2(3))// 5 + 3 = 8
+//class CloClass{
+//    var num = 6
+//    func plus(_ v:Int) -> Int {
+//        num += v
+//        return num
+//    }
+//
+//}
+
+typealias FnClo = (Int) -> (Int,Int)
+func getFns() -> (FnClo,FnClo) {
+    var num1 = 0
+    var num2 = 0
+    func plus(_ i:Int) -> (Int,Int){
+        num1 += i
+        num2 += i << 1
+        return (num1,num2)
+    }
+    func mnus(_ i:Int) -> (Int,Int){
+        num1 -= i
+        num2 -= i << 1
+        return (num1,num2)
+    }
+    return (plus,mnus)
+}
+
+let (p,m) = getFns()
+print(p(5))//5, 5=0b101 << 1 = 0b1010 = 10
+print(m(4)) //1=5-1  6
+//print(p(3))
+//print(m(2))
+
+//m(4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Swift/test_swfit/test_swfit/ViewController2.swift
+++
@@ -1,9 +0,0 @@
-//
-//  ViewController2.swift
-//  test_swfit
-//
-//  Created by Charlie on 2019/8/12.
-//  Copyright © 2019 www.fgyong.cn. All rights reserved.
-//
-
-import Foundation
