0b1e5dd3eb54121a778b26fba17d5a0dd91f5e36
ifgyong <fgyong@fgyong.cn>
2019-08-21 15:38:27 +0800

    log

--- Swift/Swift入门到精通/TesSwift/TesSwift/main.swift
+++ Swift/Swift入门到精通/TesSwift/TesSwift/main.swift
@@ -90,25 +90,131 @@ func add(a l:Int8 = 0,r b:Int8 = 10,s c:Int8) -> Void {
 
 
 
-var age = 10
-//实际z大小
-let size = MemoryLayout<Int>.size;//MemoryLayout<Int>.size(ofValue: age)
-//分配的大小
-let stride = MemoryLayout<Int>.stride //MemoryLayout<Int>.stride(ofValue: age)
-//对其参数
-let alignment = MemoryLayout<Int>.alignment //MemoryLayout<Int>.alignment(ofValue: age)
-print("size:\(size) stride:\(stride) alignment:\(alignment)")
+//var age = 10
+////实际z大小
+//let size = MemoryLayout<Int>.size;//MemoryLayout<Int>.size(ofValue: age)
+////分配的大小
+//let stride = MemoryLayout<Int>.stride //MemoryLayout<Int>.stride(ofValue: age)
+////对其参数
+//let alignment = MemoryLayout<Int>.alignment //MemoryLayout<Int>.alignment(ofValue: age)
+//print("size:\(size) stride:\(stride) alignment:\(alignment)")
 // size:8 stride:8 alignment:8
 
-enum Password {
-	case number(Int,Int,Int,Int)
-	case other
+//enum Password {
+//	case number(Int,Int,Int,Int)
+//	case other
+//}
+//enum TestEnum {
+//	case test1,test2,test3,test4
+//}
+//var testum = TestEnum.test1
+//var p1 = MemoryLayout.stride(ofValue: testum)
+//var p2 = MemoryLayout.size(ofValue: testum)
+//var p3 = MemoryLayout.alignment(ofValue: testum)
+//print("分配：\(p1)  占用：\(p2) 对齐：\(p3)")
+
+// rip存储 下句CPU的下一条指令地址
+//  movq   $0x1, 0x5d21(%rip) 内存地址：0x0000000100001acf+0x5d1e
+//  movq   $0x2, 0x5d1e(%rip) 内存地址：
+//  movq   $0x7, 0x5d1b(%rip) 内存地址：0x0000000100001ae5+0x5d1b=0x100007800
+//  movq   $0x8, 0x5d18(%rip) 内存地址：
+//  movb   $0x0, 0x5d19(%rip) 内存地址：
+//  leaq   0x5cf2(%rip), %rdi
+//var pwd = Password.number(3, 5, 7, 8)//字节 8*4 =32
+//var otherPwd = 123
+//
+//pwd = .other//同样的变量还是32字节
+// p1 = MemoryLayout.stride(ofValue: pwd)// 40
+// p2 = MemoryLayout.size(ofValue: pwd)//33
+// p3 = MemoryLayout.alignment(ofValue: pwd)//8
+//print("分配：\(p1)  占用：\(p2) 对齐：\(p3)")
+//print(Mems.ptr(ofRef: pwd))
+//func test(){
+//	var a = 9
+//	var b = a + 2
+//
+//
+//}
+//test()
+
+/*
+let pwd5 = Password.number2(4, 5,6)
+0x100006530: 0x00000004 0x00000000 0x00000005 0x00000000
+0x100006540: 0x00000006 0x00000000 0x00000020 0x00000000
+
+let pwd5 = Password.number3(4, 5)//同样的变量还是33字节
+0x100006530: 0x00000004 0x00000000 0x00000005 0x00000000
+0x100006540: 0x00000000 0x00000000 0x00000040
+
+
+
+*/
+func testeStruct(){
+	
+	struct Point {
+		var x:Int = 0
+		var y:Int = 0
+//		var b = true
+		
+		//	init(x:Int) {
+		//		self.x = x
+		//	}
+		func copy(with zone: NSZone? = nil) -> Any {
+			return self
+		}
+		
+	}
+//	var point1 = Point()
+//	print(MemoryLayout<Point>.size(ofValue: point1))
+//	print(point1)
+	
+	
+	class PointClass {
+		var x:Int = 0
+		var y:Int = 0
+		
+		
+	}
+//	var p11 = Point(x: 6, y: 7)
+//	p11 = Point(x: 8, y: 9)
+//	var p22 = p11
+//	print("p11:",Mems.ptr(ofVal: &p11))
+//	print("p22:",Mems.ptr(ofVal: &p22))
+
+//	 movq   %rax, -0x10(%rbp) //rbp-0x10  相差8字节 就是int的空间
+//	 movq   %rdx, -0x8(%rbp) //rbp-0x8
+//	 movq   %rax, -0x20(%rbp)//rbp-0x20
+//	 movq   %rdx, -0x18(%rbp)//rbp-0x18
+
+	
+	
+	var s :String = "haha"
+	var s2 = s
+	print(Mems.ptr(ofVal: &s2),Mems.ptr(ofVal: &s))
+	s2.append("来了")
+	print(Mems.ptr(ofVal: &s2),Mems.ptr(ofVal: &s))
+
+	var set1 = Set(arrayLiteral: 1)
+	var set2 = set1
+	
+	print(Mems.ptr(ofVal: &set2),Mems.ptr(ofVal: &set1))
+
+	
+	var dic1 = Dictionary(dictionaryLiteral: ("key","value"))
+	var dic2 = dic1
+	
+	print(Mems.ptr(ofVal: &dic2),Mems.ptr(ofVal: &dic1))
+
+	
+	
+	
+	
 }
-var pwd = Password.number(1, 2, 9, 0)//字节 8*4 =32
-pwd = .other//同样的变量还是32字节
-let p1 = MemoryLayout.stride(ofValue: pwd)// 40
-let p2 = MemoryLayout.size(ofValue: pwd)//33
-let p3 = MemoryLayout.alignment(ofValue: pwd)//8
-print("\(p1) \(p2) \(p3)")
+
+
+
+testeStruct()
+
+
 
 
