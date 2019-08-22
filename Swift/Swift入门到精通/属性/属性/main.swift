572a7e6db407e74a51733f8dbe5f6a2af3ce760c
ifgyong <fgyong@fgyong.cn>
2019-08-22 22:57:12 +0800

    static本质 inout本质

--- Swift/Swift入门到精通/属性/属性/main.swift
+++ Swift/Swift入门到精通/属性/属性/main.swift
@@ -8,85 +8,104 @@
 
 import Foundation
 
-class Point {
-	var x = 0
-	var y = 0
+class Car {
+    static var name = "法拉利"
 }
+print(Car.name)
 
-//存储case 和 关联值
 
-enum Season:Int {
-	case spring=2,summer=5
-}
-//var s = Season.spring
 
-struct Circle {  //占用空间是8字节
-	//存储属性
-	var radius : Double
-	//计算属性 本质是方法
-	var diamter : Double {
-		set{
-			radius = newValue/2
-		}
-		get{
-			return radius * 2
-		}
-		
-	}
-	
-	
-}
-var cir = Circle(radius: 4)
+//
+//class Point {
+//    var x = 0
+//    var y = 0
+//}
+//
+////存储case 和 关联值
+//
+//enum Season:Int {
+//    case spring=2,summer=5
+//}
+////var s = Season.spring
+//
+//struct Circle {  //占用空间是8字节
+//    //存储属性
+//    var radius : Double
+//    //计算属性 本质是方法
+//    var diamter : Double {
+//        set{
+//            radius = newValue/2
+//        }
+//        get{
+//            return radius * 2
+//        }
+//
+//    }
+//
+//
+//}
+//var cir = Circle(radius: 4)
+////print(cir.diamter)
+//cir.radius = 8
+//cir.diamter = 9
+//var f = cir.diamter
+
 //print(cir.diamter)
-cir.radius = 8
-cir.diamter = 9
-var f = cir.diamter
-
-//print(cir.diamter)
 
 
 
 
-struct Photo {
-	
-	var age:Int {
-//		get{
-//			return height/2
-//		}
-//		set{
-//			height =  newValue * 2
-//		}
-		willSet{
-			print("will\(newValue)")
-		}
-		didSet{
-			print("didset old:\(oldValue)")
-		}
-	}
-	var height = 12
-}
-var p = Photo(age: 5, height: 6)
-
-func test(_ v: inout Int) {
-	v = 9
-}
-test(&p.age)
-
-
-class fileManger {
-	public static let  manger:fileManger = fileManger()
-	private init(){
-		print("init")
-	}
-	func close()  {
-		print("close")
-	}
-	func open()  {
-		print("open")
-	}
-}
-var file = fileManger.manger
-file.open()
-file.close()
+//struct Photo {
+//
+//    var age:Int {
+////        get{
+////            return height/2
+////        }
+////        set{
+////            height =  newValue * 2
+////        }
+//        willSet{
+//            print("will\(newValue)")
+//        }
+//        didSet{
+//            print("didset old:\(oldValue)")
+//        }
+//    }
+////    var height = 12
+//}
+//var p = Photo(age: 5)
+//
+//func test(_ v: inout Int) {
+//    v = 9
+//}
+//test(&p.age)
 
 
+//class fileManger {
+//    public static let  manger:fileManger = fileManger()
+//    private init(){//设置私有，外部不可访问
+//        print("init")
+//    }
+//    open func close()  {
+//        print("close")
+//    }
+//    public  func open()  { //mode 可访问
+//        print("open")
+//    }
+//}
+//var file = fileManger.manger
+
+//file.init()//error：init' is inaccessible due to 'private' protection level
+//
+//
+//struct Propty {
+//    var age = 10
+//    static var level = 11
+//
+//}
+//var pro = Propty()
+//pro.age = 11;
+//var level = Propty.level;
+//
+//
+//
