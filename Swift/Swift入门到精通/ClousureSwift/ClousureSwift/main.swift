6973aa86afa45398bb8c20f4165a78df44860bd1
ifgyong <fgyong@fgyong.cn>
2019-08-22 17:44:34 +0800

    闭包和inoutdemo测试代码

--- Swift/Swift入门到精通/ClousureSwift/ClousureSwift/main.swift
+++ Swift/Swift入门到精通/ClousureSwift/ClousureSwift/main.swift
@@ -8,35 +8,53 @@
 
 import Foundation
 
-//var fn = {
-//	(v:Int) -> Void in
-//	print(v)
+
+//class Person {
+//	var age = 9
 //}
-//
-//func test(v1:Int,v2:Int,cl:(_ v:Int,_ v2:Int)->Int) -> Void {
-//	print(cl(v1,v2))
-//}
-//test(v1: 1, v2: 2) {$0+$1}
-//test(v1: 2, v2: 2) { (a, b)  in return a+b }
-//var array = [2,4,3,2,5,8,7,6,0]
-//array.sort { (a, b) -> Bool in
-//	return	a>b
-//}
-//
-//print(array)
-//typeAlias Fn = (Int) -> Int;
 //typealias Clo = (Int) -> Int
 //func getFunc () -> Clo {
-//    var num = 5 //局部变量 赋值到了堆空间来保证变量的值。每次调用都访问了堆空间了。
-//    func plus(_ v:Int) ->Int{
-//        num += v
-//        return num
-//    }
-//    return plus
+//	//    var num = 5 //局部变量 赋值到了堆空间来保证变量的值。每次调用都访问了堆空间了。
+//	//作业 person = Person() 观察 一个或者两个person
+//	var p = Person()
+//
+//	func plus(_ v:Int) ->Int{
+//		//        num += v// 申请8+8+8 =24字节在堆空间
+//		p.age += v;
+//		return p.age
+//	}
+//	print("return plus begin")
+//	return plus
 //}
-//var f = getFunc()
-//print(f(1)) //5+1 = 6
+//var f = getFunc() //
+//print(f(3))
+//
+//print("person func end")
+
+
+//func getFirstPositive2(_ v:Int,_ v2 : () -> Int) ->Int{
+//	return v > 0 ? v: v2()
+//}
+////自动闭包延迟执行闭包代码
+//func getFirstPositive(_ v:Int,_ v2 : @autoclosure () -> Int) ->Int{
+//	return v > 0 ? v: v2()
+//}
+////let a = getFIrstPositive(1,{2})
+//let a = getFirstPositive(0, 2)
+//
+//print(a)
+
+
+
+////print(MemoryLayout.stride(ofValue: f))//16
+//f(6) //5+1 = 6 调用f的前8个字节
 //print(f(2)) //6 + 2 =8
+
+
+//func sum(_ v1:Int,_ v2:Int) -> Int{return v2 + v1 }
+//var s1 = sum
+//print(s1(1,2))
+
 //
 //var f2 = getFunc()//每次调用都会调用新的堆空间的 num，他们是分开没联系的。
 //print(f2(3))// 5 + 3 = 8
@@ -50,29 +68,34 @@ import Foundation
 //
 //}
 
-typealias FnClo = (Int) -> (Int,Int)
-func getFns() -> (FnClo,FnClo) {
-    var num1 = 0
-    var num2 = 0
-    func plus(_ i:Int) -> (Int,Int){
-        num1 += i
-        num2 += i << 1
-        return (num1,num2)
-    }
-    func mnus(_ i:Int) -> (Int,Int){
-        num1 -= i
-        num2 -= i << 1
-        return (num1,num2)
-    }
-    return (plus,mnus)
+
+
+func cloTest1()  {
+	typealias FnClo = (Int) -> (Int,Int)
+	func getFns() -> (FnClo,FnClo) {
+	    var num1 = 1
+	    var num2 = 2
+	    func plus(_ i:Int) -> (Int,Int){
+	        num1 += i
+	        num2 += i << 1
+	        return (num1,num2)
+	    }
+	    func mnus(_ i:Int) -> (Int,Int){
+	        num1 -= i
+	        num2 -= i << 1
+	        return (num1,num2)
+	    }
+		//在return的时候分配堆空间存放num1和num2。
+	    return (plus,mnus)
+	}
+	
+	let (p,m) = getFns()
+	p(5)//(5,10) 5 = 0b101 << 1 = 0b1010 = 10
+	m(4) //(1,6) = 5-1  6
+	p(3)//(4, 8)
+	m(2)//(2, 4)
 }
-
-let (p,m) = getFns()
-print(p(5))//5, 5=0b101 << 1 = 0b1010 = 10
-print(m(4)) //1=5-1  6
-//print(p(3))
-//print(m(2))
-
+cloTest1()
 //m(4)
 
 
---
+++ Swift/Swift入门到精通/属性/属性/main.swift
@@ -0,0 +1,92 @@
+//
+//  main.swift
+//  属性
+//
+//  Created by Charlie on 2019/8/22.
+//  Copyright © 2019 www.fgyong.cn. All rights reserved.
+//
+
+import Foundation
+
+class Point {
+	var x = 0
+	var y = 0
+}
+
+//存储case 和 关联值
+
+enum Season:Int {
+	case spring=2,summer=5
+}
+//var s = Season.spring
+
+struct Circle {  //占用空间是8字节
+	//存储属性
+	var radius : Double
+	//计算属性 本质是方法
+	var diamter : Double {
+		set{
+			radius = newValue/2
+		}
+		get{
+			return radius * 2
+		}
+		
+	}
+	
+	
+}
+var cir = Circle(radius: 4)
+//print(cir.diamter)
+cir.radius = 8
+cir.diamter = 9
+var f = cir.diamter
+
+//print(cir.diamter)
+
+
+
+
+struct Photo {
+	
+	var age:Int {
+//		get{
+//			return height/2
+//		}
+//		set{
+//			height =  newValue * 2
+//		}
+		willSet{
+			print("will\(newValue)")
+		}
+		didSet{
+			print("didset old:\(oldValue)")
+		}
+	}
+	var height = 12
+}
+var p = Photo(age: 5, height: 6)
+
+func test(_ v: inout Int) {
+	v = 9
+}
+test(&p.age)
+
+
+class fileManger {
+	public static let  manger:fileManger = fileManger()
+	private init(){
+		print("init")
+	}
+	func close()  {
+		print("close")
+	}
+	func open()  {
+		print("open")
+	}
+}
+var file = fileManger.manger
+file.open()
+file.close()
+
+
