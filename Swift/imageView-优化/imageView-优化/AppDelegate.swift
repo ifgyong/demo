f972cac54e49b35ded05053e76508f9269af6101
ifgyong <fgyong@fgyong.cn>
2019-11-28 18:09:15 +0800

    file

---
+++ Swift/imageView-优化/imageView-优化/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  imageView-优化
+//
+//  Created by Charlie on 2019/11/28.
+//  Copyright © 2019 www.fgyong.cn. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+
+}
+
---
+++ Swift/imageView-优化/imageView-优化/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  imageView-优化
+//
+//  Created by Charlie on 2019/11/28.
+//  Copyright © 2019 www.fgyong.cn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
